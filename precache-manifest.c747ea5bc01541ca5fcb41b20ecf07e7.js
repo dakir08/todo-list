e4659c581d99994a17b56cb19d56e94c38085050
Yuuki <minhphuongsony@gmail.com>
2020-07-06 20:23:10 +1000

    Updates

---
+++ precache-manifest.c747ea5bc01541ca5fcb41b20ecf07e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8bd08e30b458bedf56e8d1ff71965c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd999c75549cc1a59dd0",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "176e53cc1881ad10a65d",
+    "url": "./static/js/2.e51e96cf.chunk.js"
+  },
+  {
+    "revision": "899705a3fc35f9c0c883e66fdf555456",
+    "url": "./static/js/2.e51e96cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd999c75549cc1a59dd0",
+    "url": "./static/js/main.29b1b487.chunk.js"
+  },
+  {
+    "revision": "5336c6d6012af300fbcd",
+    "url": "./static/js/runtime-main.83a5fe9d.js"
+  }
+]);
